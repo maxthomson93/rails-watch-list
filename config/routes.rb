24b44ff42333078901cdc57ca2ac6a8d0ee9ae0a
Max Thomson <maxthelaowai@gmail.com>
2025-05-16 14:58:56 +0900

    added index page root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get "up" => "rails/health#show", as: :rails_health_check
+
+  root "lists#index"
   resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create]
+  git resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
 end
