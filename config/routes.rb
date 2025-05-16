546d46d717cd03c604fa21a9b57b67c18175b198
Max Thomson <maxthelaowai@gmail.com>
2025-05-16 15:04:59 +0900

    adding root to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root "lists#index"
+  root 'lists#index'
   resources :lists, only: [:index, :show, :new, :create] do
   git resources :bookmarks, only: [:new, :create]
   end
